bdac14feed8c0458a3ef7097ff4a37a45d5acc10
thongvm316 <vominhthong1603@gmail.com>
2021-11-12 15:45:09 +0700

    20211112-2

---
+++ jotto/src/actions/index.test.js
@@ -0,0 +1,37 @@
+import moxios from 'moxios'
+import { getSecretWord } from './'
+
+describe('getSecretWord', () => {
+  beforeEach(() => {
+    moxios.install()
+  })
+
+  afterEach(() => {
+    moxios.uninstall()
+  })
+
+  test('secretWord is returned', () => {
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent()
+      request.respondWith({
+        status: 200,
+        response: 'party',
+      })
+    })
+
+    return getSecretWord().then((secretWord) => {
+      expect(secretWord).toBe('party')
+    })
+
+    /**  
+     * @Question 
+        - Config moxios - make a request and response
+        - Return redux action - and in this action, use real axios and api from server
+
+        How do this test work and return result?
+
+     * @Response
+        - Flow: test -> function -> axios -> test -> use moxios for test, not call http request here although getSecretWord() was call a http request
+    */
+  })
+})
